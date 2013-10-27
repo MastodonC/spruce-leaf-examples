e1bf6fee72bb5fcab8ffb1d45f90c551b34bf94f
Anna Pawlicka <anna@mastodonc.com>
2013-10-27 17:01:50 +0000

    Update page menu

--- js/page-menu.js
+++ js/page-menu.js
@@ -1,4 +1,4 @@
 $( '#pagemenu' ).append(
     '<li><a href="index.html">Home</a></li>' +
         '<li><a href="../diabetes_spend.html">Diabetes Spend</a></li>' +
-        '<li><a href="../diabetes_prevalence_gp_level.html">Diabetes Prevalence</a></li>');
+        '<li><a href="../multiple_geographies.html">Diabetes Prevalence</a></li>');
