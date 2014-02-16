bf7e4cf39c1a3df193d09f644b4e6fe63af5eda8
Anna Pawlicka <anna@mastodonc.com>
2014-02-16 11:39:56 +0000

    Create live example.

---
+++ js/page-menu.js
@@ -0,0 +1,4 @@
+$( '#pagemenu' ).append(
+    '<li><a href="index.html">Home</a></li>' +
+        '<li><a href="../diabetes_spend.html">Diabetes Spend</a></li>' +
+        '<li><a href="../multiple_geographies.html">Diabetes Prevalence</a></li>');
